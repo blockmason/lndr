a586f3b6ba98e66910860b3161e785c35e43cf59
Roy Blankman <riblankman@gmail.com>
2017-10-21 15:21:49 -0400

    using appropriate address types for IssueCreditLog

--- fid-cli/src/FiD/Cli/Main.hs
+++ fid-cli/src/FiD/Cli/Main.hs
@@ -9,7 +9,8 @@ module FiD.Cli.Main
     ) where
 
 import           Data.Either.Combinators (rightToMaybe)
-import           Data.Text
+import           Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Text.Lazy as LT
 import           Dhall hiding (Text)
 import           Network.Ethereum.Web3
@@ -28,9 +29,9 @@ data FiDCmd = Info    {config :: Text, scope :: Text}
 --  should they all be integers? why not?
 --  is there aleardy an efficient uint256 type in haskell?
 data IssueCreditLog = IssueCreditLog { ucac :: Address
-                                     , creditor :: Text
-                                     , debtor :: Text
-                                     , amount :: Text
+                                     , creditor :: Address
+                                     , debtor :: Address
+                                     , amount :: Text -- TOOD Uint256
                                      } deriving Show
 
 data FiDConfig = FiDConfig { fidAddress :: Text
@@ -58,7 +59,7 @@ allLogs = eth_getLogs (Filter Nothing Nothing (Just "0x0") Nothing)
 
 -- fetch cp logs related to FiD UCAC
 -- verify that these are proper logs
-fidLogs :: Provider a => FiDConfig -> Web3 a [IssueCreditLog]
+fidLogs :: Provider a => FiDConfig -> Web3 a [Either String IssueCreditLog]
 fidLogs config = fmap interpretUcacLog <$>
     -- TODO throw and error if `Addr.fromText` returns `Left`
     eth_getLogs (Filter (rightToMaybe . Addr.fromText $ cpAddress config)
@@ -66,11 +67,18 @@ fidLogs config = fmap interpretUcacLog <$>
                         (Just "0x0") -- start from block 0
                         Nothing)
 
-interpretUcacLog :: Change -> IssueCreditLog
-interpretUcacLog change = IssueCreditLog (changeAddress change)
-                                         ""
-                                         ""
-                                         (changeData change)
+-- transforms the standard ('0x' + 64-char) bytes32 rendering of a log field into the
+-- 40-char hex representation of an address
+bytes32ToAddress :: Text -> Text
+bytes32ToAddress = T.drop 26
+
+interpretUcacLog :: Change -> Either String IssueCreditLog
+interpretUcacLog change = do creditorAddr <- Addr.fromText . bytes32ToAddress . (!! 2) $ changeTopics change
+                             debtorAddr <- Addr.fromText . bytes32ToAddress . (!! 3) $ changeTopics change
+                             pure $ IssueCreditLog (changeAddress change)
+                                                   creditorAddr
+                                                   debtorAddr
+                                                   (changeData change)
 
 -- fetch cp logs related to a particular users use of the FiD UCAC
 

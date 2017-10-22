9f696c39e311397b378a2ff28d440cbfbd4b9155
Roy Blankman <riblankman@gmail.com>
2017-10-21 20:54:45 -0400

    user-specific logs function

--- fid-cli/src/FiD/Cli/Main.hs
+++ fid-cli/src/FiD/Cli/Main.hs
@@ -8,6 +8,7 @@ module FiD.Cli.Main
     ( main
     ) where
 
+import           Data.Aeson
 import           Data.Either.Combinators (rightToMaybe)
 import           Data.Text (Text)
 import qualified Data.Text as T
@@ -42,21 +43,26 @@ data FiDConfig = FiDConfig { fidAddress :: Text
 
 instance Interpret FiDConfig
 
+
 main :: IO ()
 main = do mode <- cmdArgs (modes [Info "" "fid", Request "" "" 0, Send "" "" 0])
           let configFilePath = config mode
           config <- input auto $ LT.fromStrict configFilePath
           runMode config mode
 
+
 runMode :: FiDConfig -> FiDCmd -> IO ()
 runMode config (Info _ "fid") = print =<< runWeb3 (fidLogs config)
-runMode _ (Info _ "all") = print =<< runWeb3 allLogs
+runMode _      (Info _ "all") = print =<< runWeb3 allLogs
+runMode config (Info _ "user") = print =<< runWeb3 (userLogs config)
 runMode _ _ = putStrLn "Not yet implemented"
+
 
 -- fetch all logs
 -- terminal equivalent: curl -X POST --data {"jsonrpc":"2.0","method":"eth_getLogs","params":[{"fromBlock": "0x0"}],"id":73} localhost:8545
 allLogs :: Provider a => Web3 a [Change]
 allLogs = eth_getLogs (Filter Nothing Nothing (Just "0x0") Nothing)
+
 
 -- fetch cp logs related to FiD UCAC
 -- verify that these are proper logs
@@ -68,15 +74,37 @@ fidLogs config = fmap interpretUcacLog <$>
                         (Just "0x0") -- start from block 0
                         Nothing)
 
+
+-- TODO throw and error if `Addr.fromText` returns `Left`
+userLogs :: Provider a => FiDConfig -> Web3 a [Either String IssueCreditLog]
+userLogs config = do asCreditor <- fmap interpretUcacLog <$> eth_getLogs credFilter
+                     asDebtor <- fmap interpretUcacLog <$> eth_getLogs debtFilter
+                     return $ asCreditor ++ asDebtor
+    where filterWithTopics topics =
+                       Filter (rightToMaybe . Addr.fromText $ cpAddress config)
+                              (Just topics)
+                              (Just "0x0")
+                              Nothing
+          credFilter = filterWithTopics [Nothing, Nothing, Just userAddrBytes, Nothing]
+          debtFilter = filterWithTopics [Nothing, Nothing, Nothing, Just userAddrBytes]
+          userAddrBytes = addressToBytes32 $ userAddress config
+
+
 -- transforms the standard ('0x' + 64-char) bytes32 rendering of a log field into the
 -- 40-char hex representation of an address
 bytes32ToAddress :: Text -> Text
 bytes32ToAddress = T.drop 26
 
+addressToBytes32 :: Text -> Text
+addressToBytes32 = T.append "0x000000000000000000000000" . T.drop 2
+
+
+
 hexToInteger :: Text -> Integer
 hexToInteger = fst . head . readHex . dropHexPrefix . T.unpack
     where dropHexPrefix ('0' : 'x' : xs) = xs
           dropHexPrefix xs = xs
+
 
 interpretUcacLog :: Change -> Either String IssueCreditLog
 interpretUcacLog change = do creditorAddr <- Addr.fromText . bytes32ToAddress . (!! 2) $ changeTopics change
@@ -85,7 +113,3 @@ interpretUcacLog change = do creditorAddr <- Addr.fromText . bytes32ToAddress . 
                                                    creditorAddr
                                                    debtorAddr
                                                    (hexToInteger $ changeData change)
-
--- fetch cp logs related to a particular users use of the FiD UCAC
-
--- fetch cp logs related to a particular users use of the FiD UCAC

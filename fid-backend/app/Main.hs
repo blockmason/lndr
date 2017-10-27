32c0663dd8008a3674796e675cc969b569ec3530
Roy Blankman <riblankman@gmail.com>
2017-10-27 02:06:03 -0400

    pending removal post finalization

--- fid-backend/app/Main.hs
+++ fid-backend/app/Main.hs
@@ -59,22 +59,24 @@ submitHandler record@(CreditRecord creditor _ _ user) = do
     val <- liftIO . atomically $ Map.lookup hash creditMap
 
     case val of
-        Just storedRec -> liftIO $ if signature storedRec == signature signedRecord
+        Just storedRecord -> liftIO $ if signature storedRecord == signature signedRecord
 
             -- if the credit record has already been seen, do nothing
             then return ()
 
             -- if the submitted credit record has a matching pending record,
             -- finalize the transaction on the blockchain
-            else void $ if creditor /= user
-                    then do print "debtor finalized"
-                            finalizeTransaction (signature storedRec)
-                                                (signature signedRecord)
-                                                signedRecord
-                    else do print "creditor finalized"
-                            finalizeTransaction (signature signedRecord)
-                                                (signature storedRec)
-                                                signedRecord
+            else do if creditor /= user
+                        then do print "debtor finalized"
+                                finalizeTransaction (signature storedRecord)
+                                                    (signature signedRecord)
+                                                    signedRecord
+                        else do print "creditor finalized"
+                                finalizeTransaction (signature signedRecord)
+                                                    (signature storedRecord)
+                                                    signedRecord
+                    -- delete pending record after transaction finalization
+                    atomically $ Map.delete hash creditMap
 
         -- if no matching transaction is found, create pending transaction
         Nothing -> liftIO . atomically $ Map.insert signedRecord hash creditMap

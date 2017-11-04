6b3f50c413a0b276e5c3cb9b564562c9c4520770
Roy Blankman <riblankman@gmail.com>
2017-11-04 19:16:14 -0400

    making basic docs

--- fid-backend/src/Docs.hs
+++ fid-backend/src/Docs.hs
@@ -7,53 +7,54 @@
 
 module Docs where
 
-import Data.ByteString.Lazy (ByteString)
-import Data.Proxy
-import Data.Text.Lazy.Encoding (encodeUtf8)
-import Data.Text.Lazy (pack)
-import Network.HTTP.Types
-import Network.Wai
-import Servant.API
-import Servant.Docs
-import Servant.Server
+import           Data.ByteString.Lazy (ByteString)
+import           Data.Proxy
+import           Data.Text (Text)
+import           Data.Text.Lazy.Encoding (encodeUtf8)
+import           Data.Text.Lazy (pack)
+import           Network.HTTP.Types
+import           Network.Wai
+import           Servant.API
+import           Servant.Docs
+import           Servant.Server
 
-import Server
+import           Server
+import           EthInterface
 
--- instance ToCapture (Capture "x" Int) where
---   toCapture _ =
---     DocCapture "x"                                -- name
---                "(integer) position on the x axis" -- description
---
--- instance ToCapture (Capture "y" Int) where
---   toCapture _ =
---     DocCapture "y"                                -- name
---                "(integer) position on the y axis" -- description
---
--- instance ToSample Position Position where
---   toSample _ = Just (Position 3 14) -- example of output
---
--- instance ToParam (QueryParam "name" String) where
---   toParam _ =
---     DocQueryParam "name"                     -- name
---                   ["Alp", "John Doe", "..."] -- example of values (not necessarily exhaustive)
---                   "Name of the person to say hello to." -- description
---                   Normal -- Normal, List or Flag
---
--- instance ToSample HelloMessage HelloMessage where
---   toSamples _ =
---     [ ("When a value is provided for 'name'", HelloMessage "Hello, Alp")
---     , ("When 'name' is not specified", HelloMessage "Hello, anonymous coward")
---     ]
---     -- mutliple examples to display this time
---
--- ci :: ClientInfo
--- ci = ClientInfo "Alp" "alp@foo.com" 26 ["haskell", "mathematics"]
---
--- instance ToSample ClientInfo ClientInfo where
---   toSample _ = Just ci
---
--- instance ToSample Email Email where
---   toSample _ = Just (emailForClient ci)
+creditHash :: Text
+creditHash = "0x7e2e9ff3a5fc148cf76261755c4c666630bfc3a28d02733cfbe721fc965aca28"
 
--- apiDocs :: API
--- apiDocs = docs fiddyAPI
+crSigned :: CreditRecord Signed
+crSigned = CreditRecord "0x11edd217a875063583dd1b638d16810c5d34d54b"
+                        "0x6a362e5cee1cf5a5408ff1e12b0bc546618dffcb"
+                        69
+                        "0x457b0db63b83199f305ef29ba2d7678820806d98abbe3f6aafe015957ecfc5892368b4432869830456c335ade4f561603499d0216cda3af7b6b6cadf6f273c101b"
+
+cr :: CreditRecord Unsigned
+cr = CreditRecord "0x11edd217a875063583dd1b638d16810c5d34d54b"
+                 "0x6a362e5cee1cf5a5408ff1e12b0bc546618dffcb"
+                 69
+                 "0x6a362e5cee1cf5a5408ff1e12b0bc546618dffcb"
+
+instance ToSample Text where
+    toSamples _ = singleSample creditHash
+
+instance ToSample (CreditRecord Signed) where
+    toSamples _ = singleSample crSigned
+
+
+instance ToSample (CreditRecord Unsigned) where
+    toSamples _ = singleSample cr
+
+instance ToSample IssueCreditLog where
+    toSamples _ = singleSample $
+        IssueCreditLog "d5ec73eac35fc9dd6c3f440bce314779fed09f60"
+                       "0x11edd217a875063583dd1b638d16810c5d34d54b"
+                       "0x6a362e5cee1cf5a5408ff1e12b0bc546618dffcb"
+                       69
+
+instance ToSample SubmissionResponse where
+    toSamples _ = singleSample $ SubmissionResponse "0x4358c649de5746c91673378dd4c40a78feda715166913e09ded45343ff76841c" 1
+
+apiDocs :: API
+apiDocs = docs fiddyAPI
--- fid-backend/src/Server.hs
+++ fid-backend/src/Server.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}

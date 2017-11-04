95f203aa104e1a8b9932d221087ab3dcb3d2d85d
Roy Blankman <riblankman@gmail.com>
2017-11-04 18:05:19 -0400

    module reorg

---
+++ fid-backend/src/Docs.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Docs where
+
+import Data.ByteString.Lazy (ByteString)
+import Data.Proxy
+import Data.Text.Lazy.Encoding (encodeUtf8)
+import Data.Text.Lazy (pack)
+import Network.HTTP.Types
+import Network.Wai
+import Servant.API
+import Servant.Docs
+import Servant.Server
+
+-- instance ToCapture (Capture "x" Int) where
+--   toCapture _ =
+--     DocCapture "x"                                -- name
+--                "(integer) position on the x axis" -- description
+--
+-- instance ToCapture (Capture "y" Int) where
+--   toCapture _ =
+--     DocCapture "y"                                -- name
+--                "(integer) position on the y axis" -- description
+--
+-- instance ToSample Position Position where
+--   toSample _ = Just (Position 3 14) -- example of output
+--
+-- instance ToParam (QueryParam "name" String) where
+--   toParam _ =
+--     DocQueryParam "name"                     -- name
+--                   ["Alp", "John Doe", "..."] -- example of values (not necessarily exhaustive)
+--                   "Name of the person to say hello to." -- description
+--                   Normal -- Normal, List or Flag
+--
+-- instance ToSample HelloMessage HelloMessage where
+--   toSamples _ =
+--     [ ("When a value is provided for 'name'", HelloMessage "Hello, Alp")
+--     , ("When 'name' is not specified", HelloMessage "Hello, anonymous coward")
+--     ]
+--     -- mutliple examples to display this time
+--
+-- ci :: ClientInfo
+-- ci = ClientInfo "Alp" "alp@foo.com" 26 ["haskell", "mathematics"]
+--
+-- instance ToSample ClientInfo ClientInfo where
+--   toSample _ = Just ci
+--
+-- instance ToSample Email Email where
+--   toSample _ = Just (emailForClient ci)
+--
+-- apiDocs :: API
+-- apiDocs = docs exampleAPI
---
+++ fid-backend/src/Server.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Server where
+
+import           Control.Concurrent.STM
+import           Control.Monad.Reader
+import           Control.Monad.Trans.Either
+import           Data.Text (Text)
+import qualified Data.Text as T
+import           ListT
+import           Network.Ethereum.Web3
+import           Servant
+import qualified STMContainers.Map as Map
+
+import           EthInterface
+
+type ServerState = Map.Map Text (CreditRecord Signed)
+
+freshState :: forall k v. IO (Map.Map k v)
+freshState = atomically Map.new
+
+type API = "transactions" :> Get '[JSON] [IssueCreditLog]
+      :<|> "pending" :> Get '[JSON] [(Text, CreditRecord Signed)]
+      :<|> "submit" :> ReqBody '[JSON] (CreditRecord Unsigned) :> Post '[JSON] SubmissionResponse
+
+server :: ServerT API (ReaderT ServerState IO)
+server = transactionsHandler
+    :<|> pendingHandler
+    :<|> submitHandler
+
+
+transactionsHandler :: ReaderT ServerState IO [IssueCreditLog]
+transactionsHandler = do
+    a <- runWeb3 fidLogs
+    return $ case a of
+                Right ls -> ls
+                Left _ -> []
+
+pendingHandler :: ReaderT ServerState IO [(Text, CreditRecord Signed)]
+pendingHandler = do creditMap <- ask
+                    liftIO . atomically . toList $ Map.stream creditMap
+
+submitHandler :: CreditRecord Unsigned -> ReaderT ServerState IO SubmissionResponse
+submitHandler record@(CreditRecord creditor _ _ user) = do
+    creditMap <- ask
+    -- TODO handle this appropriately
+    Right (nonce, hash, signedRecord) <- liftIO $ signCreditRecord record
+
+    -- check if hash is already registered in pending txs
+    val <- liftIO . atomically $ Map.lookup hash creditMap
+
+    case val of
+        Just storedRecord -> liftIO . when (signature storedRecord /= signature signedRecord) $ do
+            -- if the submitted credit record has a matching pending record,
+            -- finalize the transaction on the blockchain
+            if creditor /= user
+                then finalizeTransaction (signature storedRecord)
+                                         (signature signedRecord)
+                                         signedRecord
+                else finalizeTransaction (signature signedRecord)
+                                         (signature storedRecord)
+                                         signedRecord
+            -- delete pending record after transaction finalization
+            atomically $ Map.delete hash creditMap
+
+        -- if no matching transaction is found, create pending transaction
+        Nothing -> liftIO . atomically $ Map.insert signedRecord hash creditMap
+
+    return $ SubmissionResponse hash nonce

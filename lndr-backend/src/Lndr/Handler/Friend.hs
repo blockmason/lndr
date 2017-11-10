07df4220e15195d1414df2199bf4ce67b19f3e8f
Roy Blankman <riblankman@gmail.com>
2017-11-10 12:29:00 -0500

    finishing add/removeFriendsHandler impls

--- lndr-backend/src/Lndr/Handler/Friend.hs
+++ lndr-backend/src/Lndr/Handler/Friend.hs
@@ -2,6 +2,7 @@ module Lndr.Handler.Friend where
 
 import           Control.Monad.Reader
 import           Control.Concurrent.STM
+import           Data.List ((\\), nub)
 import           Data.Maybe (fromMaybe)
 import           Lndr.Handler.Types
 import           Lndr.Types
@@ -17,24 +18,28 @@ nickHandler (NickRequest addr nick sig) = do
     return NoContent
 
 
+lookupFriends :: Address -> Map.Map Address [Address] -> LndrHandler [Address]
+lookupFriends x y = fmap (fromMaybe []) . liftIO . atomically $ Map.lookup x y
+
+
 friendHandler :: Address -> LndrHandler [Address]
 friendHandler addr = do
     friendListMapping <- friendlistMap <$> ask
-    fmap (fromMaybe []) . liftIO . atomically $ Map.lookup addr friendListMapping
+    lookupFriends addr friendListMapping
 
 
 addFriendsHandler :: Address -> [Address] -> LndrHandler NoContent
-addFriendsHandler addr removes = do
+addFriendsHandler addr adds = do
     -- TODO verify signature
     friendListMapping <- friendlistMap <$> ask
-    -- TODO use same function as friendHandler, then modify list, then update
-    -- the mapping
+    friendList <- lookupFriends addr friendListMapping
+    liftIO . atomically $ Map.insert (nub $ friendList ++ adds) addr friendListMapping
     return NoContent
 
 removeFriendsHandler :: Address -> [Address] -> LndrHandler NoContent
-removeFriendsHandler addr adds = do
+removeFriendsHandler addr removes = do
     -- TODO verify signature
     friendListMapping <- friendlistMap <$> ask
-    -- TODO use same function as friendHandler, then modify list, then update
-    -- the mapping
+    friendList <- lookupFriends addr friendListMapping
+    liftIO . atomically $ Map.insert (friendList \\ removes) addr friendListMapping
     return NoContent

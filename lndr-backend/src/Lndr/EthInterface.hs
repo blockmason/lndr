ff60616e142aabb3bd1cb0ada46e0cd013cafb00
Roy Blankman <riblankman@gmail.com>
2017-11-17 13:30:40 -0500

    getting rid of silly intlit

--- lndr-backend/src/Lndr/EthInterface.hs
+++ lndr-backend/src/Lndr/EthInterface.hs
@@ -86,7 +86,7 @@ queryBalance :: Address -> IO (Either Web3Error Integer)
 queryBalance = runWeb3 . fmap adjustSigned . balances cpAddr ucacIdB
     where adjustSigned x | x > div maxNeg 2 = x - maxNeg
                          | otherwise        = x
-          maxNeg = 115792089237316195423570985008687907853269984665640564039457584007913129639936
+          maxNeg = 2^256
 
 
 queryNonce :: Provider a => Address -> Address -> Web3 a Integer

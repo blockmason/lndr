d2d9da717c16d692ff818dcad804adef89270aad
Roy Blankman <riblankman@gmail.com>
2017-11-30 22:54:48 -0500

    apapting deployment migration for new CreditProtocol.sol

--- ucac/migrations/2_deploy_contracts.js
+++ ucac/migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const CreditProtocol = artifacts.require('credit-protocol/contracts/CreditProtocol.sol');
 const CPToken = artifacts.require('tce-contracts/contracts/CPToken.sol');
-const FriendInDebt = artifacts.require('./FriendInDebt.sol');
+const Lndr = artifacts.require('./Lndr.sol');
 
 function sign(signer, content) {
     let contentHash = web3.sha3(content, {encoding: 'hex'});
@@ -19,29 +19,37 @@ function sign(signer, content) {
     return res;
 }
 
+
+function stripHex(addr) {
+    return addr.substr(2, addr.length);
+}
+
+function creditHash(ucacAddr, p1, p2, amount, nonce) {
+    return [ucacAddr, p1, p2, amount, nonce].map(stripHex).join("")
+}
+
 function bignumToHexString(num) {
     const a = num.toString(16);
     return "0x" + '0'.repeat(64 - a.length) + a;
 }
 
-async function makeTransaction(cp, ucacId, creditor, debtor, _amount) {
+async function makeTransaction(cp, ucacAddr, creditor, debtor, _amount) {
     let nonce = creditor < debtor ? await cp.nonces(creditor, debtor) : await cp.nonces(debtor, creditor);
     nonce = bignumToHexString(nonce);
     let amount = bignumToHexString(_amount);
-    let content = ucacId + creditor.substr(2, creditor.length) + debtor.substr(2, debtor.length)
-                          + amount.substr(2, amount.length) + nonce.substr(2, nonce.length);
+    let content = creditHash(ucacAddr, creditor, debtor, amount, nonce);
     let sig1 = sign(creditor, content);
     let sig2 = sign(debtor, content);
-    let txReciept = await cp.issueCredit( ucacId, creditor, debtor, amount
-                                   , sig1.r, sig1.s, sig1.v
-                                   , sig2.r, sig2.s, sig2.v, {from: creditor});
+    let txReciept = await cp.issueCredit( ucacAddr, creditor, debtor, amount
+                                   , [ sig1.r, sig1.s, sig1.v ]
+                                   , [ sig2.r, sig2.s, sig2.v ]
+                                   , testMemo, {from: creditor});
     return txReciept;
 }
 
 module.exports = function(deployer, network, accounts) {
     if (network == "gethtest") {
         const usd = web3.fromAscii("USD");
-        const ucacId1 = web3.sha3("hi");
         const mintAmount = web3.toBigNumber(web3.toWei(20000))
         var cpTokenContract;
         var creditProtocolContract;
@@ -60,9 +68,9 @@ module.exports = function(deployer, network, accounts) {
             return CreditProtocol.deployed();
         }).then(_creditProtocolContract => {
             creditProtocolContract = _creditProtocolContract;
-            return deployer.deploy(FriendInDebt, {from: web3.eth.accounts[0]});
+            return deployer.deploy(Lndr, {from: web3.eth.accounts[0]});
         }).then(() => {
-            return FriendInDebt.deployed();
+            return Lndr.deployed();
         }).then(_fidContract => {
             fidContract = _fidContract;
             // mint tokens for accounts[0] to stake fid
@@ -77,7 +85,6 @@ module.exports = function(deployer, network, accounts) {
             return cpTokenContract.approve(creditProtocolContract.address, web3.toWei(100), {from: web3.eth.accounts[0]});
         }).then(() => {
             return creditProtocolContract.createAndStakeUcac( fidContract.address
-                                                            , ucacId1
                                                             , usd
                                                             , web3.toWei(100)
                                                             , {from: web3.eth.accounts[0]});

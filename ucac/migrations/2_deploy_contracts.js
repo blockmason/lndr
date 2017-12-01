79eadc5a8a63e9c865e618627f76df806edbcea5
Roy Blankman <riblankman@gmail.com>
2017-11-30 23:06:34 -0500

    cleanup migrations file, update config for local tests

--- ucac/migrations/2_deploy_contracts.js
+++ ucac/migrations/2_deploy_contracts.js
@@ -4,49 +4,6 @@ const CreditProtocol = artifacts.require('credit-protocol/contracts/CreditProtoc
 const CPToken = artifacts.require('tce-contracts/contracts/CPToken.sol');
 const Lndr = artifacts.require('./Lndr.sol');
 
-function sign(signer, content) {
-    let contentHash = web3.sha3(content, {encoding: 'hex'});
-    let sig = web3.eth.sign(signer, contentHash, {encoding: 'hex'});
-    sig = sig.substr(2, sig.length);
-
-    let res = {};
-    res.r = "0x" + sig.substr(0, 64);
-    res.s = "0x" + sig.substr(64, 64);
-    res.v = web3.toDecimal("0x" + sig.substr(128, 2));
-
-    if (res.v < 27) res.v += 27;
-
-    return res;
-}
-
-
-function stripHex(addr) {
-    return addr.substr(2, addr.length);
-}
-
-function creditHash(ucacAddr, p1, p2, amount, nonce) {
-    return [ucacAddr, p1, p2, amount, nonce].map(stripHex).join("")
-}
-
-function bignumToHexString(num) {
-    const a = num.toString(16);
-    return "0x" + '0'.repeat(64 - a.length) + a;
-}
-
-async function makeTransaction(cp, ucacAddr, creditor, debtor, _amount) {
-    let nonce = creditor < debtor ? await cp.nonces(creditor, debtor) : await cp.nonces(debtor, creditor);
-    nonce = bignumToHexString(nonce);
-    let amount = bignumToHexString(_amount);
-    let content = creditHash(ucacAddr, creditor, debtor, amount, nonce);
-    let sig1 = sign(creditor, content);
-    let sig2 = sign(debtor, content);
-    let txReciept = await cp.issueCredit( ucacAddr, creditor, debtor, amount
-                                   , [ sig1.r, sig1.s, sig1.v ]
-                                   , [ sig2.r, sig2.s, sig2.v ]
-                                   , testMemo, {from: creditor});
-    return txReciept;
-}
-
 module.exports = function(deployer, network, accounts) {
     if (network == "gethtest") {
         const usd = web3.fromAscii("USD");

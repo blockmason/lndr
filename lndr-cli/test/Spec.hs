cb2ca1d7b3859b2009ba1260694679dc19dfea84
Roy Blankman <riblankman@gmail.com>
2017-12-09 21:05:03 -0500

    adding duplicate nick test

--- lndr-cli/test/Spec.hs
+++ lndr-cli/test/Spec.hs
@@ -14,23 +14,34 @@ testUrl = "http://localhost:80"
 
 testAddress = "198e13017d2333712bd942d8b028610b95c363da"
 
+testNick = "test"
+
 main :: IO ()
 main = defaultMain tests
 
 
 tests :: [Test]
-tests = [ testCase "nick" nickTest
-        , testCase "add friend" addFriendTest
+tests = [ testGroup "Signing"
+            [ testCase "setting nick" setNickTest
+            , testCase "duplicate nick rejected" duplicateNickTest
+            ]
+        , testGroup "Friends"
+            [ testCase "add friend" addFriendTest ]
         ]
 
 
-nickTest :: Assertion
-nickTest = do
+setNickTest :: Assertion
+setNickTest = do
     setNick testUrl (NickRequest testAddress testNick "")
     queriedNick <- getNick testUrl testAddress
     assertEqual "nick is set and queryable" queriedNick testNick
-    where
-        testNick = "test"
+
+
+duplicateNickTest :: Assertion
+duplicateNickTest = do
+    httpCode <- setNick testUrl (NickRequest testAddress testNick "")
+    assertEqual "duplicate nick is rejected with user error" 400 httpCode
+
 
 addFriendTest :: Assertion
 addFriendTest = putStrLn "to be implemented"

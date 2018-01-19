ab113b9dd5a130bb4f6c6b541d71bd03a536a3fb
Roy Blankman <riblankman@gmail.com>
2018-01-18 19:43:03 -0500

    adding `parseIssueCreditInput` skeleton

--- lndr-backend/src/Lndr/Util.hs
+++ lndr-backend/src/Lndr/Util.hs
@@ -131,3 +131,8 @@ creditRowToCreditRecord (creditor, debtor, amount, memo, submitter, nonce, hash,
 settlementCreditRowToCreditRecord :: (Address, Address, Integer, Text, Address, Integer, Text, Rational, Text, Rational)
                                   -> CreditRecord
 settlementCreditRowToCreditRecord (creditor, debtor, amount, memo, submitter, nonce, hash, settlementAmount, settlementCurrency, settlementBlockNumber) = CreditRecord creditor debtor amount memo submitter nonce hash "" (Just $ floor settlementAmount) (Just settlementCurrency) (Just $ floor settlementBlockNumber)
+
+-- TODO implement this
+-- example input data: 0x0a5b410e000000000000000000000000869a8f2c3d22be392618ed06c8f548d1d5b5aed600000000000000000000000070d71994d0414c19c1f09f1f2946544e8d97c4290000000000000000000000001b5fec5060e51886184d30b3d211d50836087b83000000000000000000000000000000000000000000000000000000000000006481e2e0f119561515281f1c76d64431760a8612305931f5378f3004da3aa6209927b3f24c58888a06da343aa4bf1f3520122e703e6ccf146ee9ac60e2123b10d8000000000000000000000000000000000000000000000000000000000000001bb6ff2d00768200c769018c277d53b2e2a3a3d5f4d740a2acc3e35cb4421dd38a45a02960d1e92cf061a357a819072658a482c02c4a4ee06e01cb58cf66bc3a8f000000000000000000000000000000000000000000000000000000000000001c736f6d657468696e672020202020202020202020202020202020202020202020
+parseIssueCreditInput :: B.ByteString -> (Address, Address, Address, Integer, Text, Text, Text)
+parseIssueCreditInput = undefined

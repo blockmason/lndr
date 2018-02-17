a9a03b8b404468692604a5a92659d1c6d869cf96
Roy Blankman <riblankman@gmail.com>
2018-02-17 17:21:40 -0500

    adding additional currency ucac deploys

--- ucac/migrations/2_deploy_contracts.js
+++ ucac/migrations/2_deploy_contracts.js
@@ -3,50 +3,45 @@ const fs = require('fs');
 const CreditProtocol = artifacts.require('credit-protocol/contracts/CreditProtocol.sol');
 const CPToken = artifacts.require('tce-contracts/contracts/CPToken.sol');
 const Lndr = artifacts.require('./Lndr.sol');
+const LndrJPY = artifacts.require('./LndrJPY.sol');
+const LndrKRW = artifacts.require('./LndrKRW.sol');
 
-module.exports = function(deployer, network, accounts) {
+module.exports = async function (deployer, network, accounts) {
     if (network == "gethtest" || network == "testrpc") {
         const usd = web3.fromAscii("USD");
+        const jpy = web3.fromAscii("JPY");
+        const krw = web3.fromAscii("KRW");
         const mintAmount = web3.toBigNumber(web3.toWei(20000))
         var cpTokenContract;
         var creditProtocolContract;
-        var fidContract;
+        var lndrUsdContract;
+        var lndrJpyContract;
+        var lndrKrwContract;
 
-        deployer.deploy(CPToken,{from: web3.eth.accounts[0]}).then(() => {
-            return CPToken.deployed();
-        }).then(_cpTokenContract => {
-            cpTokenContract = _cpTokenContract;
-            return deployer.deploy( CreditProtocol
-                                  , cpTokenContract.address
-                                  , web3.toBigNumber(2 * 10 ** 9)
-                                  , web3.toBigNumber(1)
+        await deployer.deploy(CPToken,{from: web3.eth.accounts[0]})
+        cpTokenContract = await CPToken.deployed();;
+        return deployer.deploy( CreditProtocol
+                              , cpTokenContract.address
+                              , web3.toBigNumber(2 * 10 ** 9)
+                              , web3.toBigNumber(1)
+                              , {from: web3.eth.accounts[0]});
+        creditProtocolContract = await CreditProtocol.deployed();
+        await deployer.deploy(Lndr, {from: web3.eth.accounts[0]});
+        await deployer.deploy(LndrJPY, {from: web3.eth.accounts[0]});
+        await deployer.deploy(LndrKRW, {from: web3.eth.accounts[0]});
+        lndrUsdContract = await Lndr.deployed();
+        lndrJpyContract = await LndrJPY.deployed();
+        lndrKrwContract = await LndrKRW.deployed();
+        // mint tokens for accounts[0] to stake fid
+        await cpTokenContract.mint( web3.eth.accounts[0]
+                                  , web3.toWei(2000)
                                   , {from: web3.eth.accounts[0]});
-        }).then(() => {
-            return CreditProtocol.deployed();
-        }).then(_creditProtocolContract => {
-            creditProtocolContract = _creditProtocolContract;
-            return deployer.deploy(Lndr, {from: web3.eth.accounts[0]});
-        }).then(() => {
-            return Lndr.deployed();
-        }).then(_fidContract => {
-            fidContract = _fidContract;
-            // mint tokens for accounts[0] to stake fid
-            return cpTokenContract.mint( web3.eth.accounts[0]
-                                       , web3.toWei(2000)
-                                       , {from: web3.eth.accounts[0]});
-        }).then(() => {
-            return cpTokenContract.finishMinting({from: web3.eth.accounts[0]});
-        }).then(() => {
-            return cpTokenContract.endSale({from: web3.eth.accounts[0]});
-        }).then(() => {
-            return cpTokenContract.approve(creditProtocolContract.address, web3.toWei(100), {from: web3.eth.accounts[0]});
-        }).then(() => {
-            return creditProtocolContract.createAndStakeUcac( fidContract.address
-                                                            , usd
-                                                            , web3.toWei(100)
-                                                            , {from: web3.eth.accounts[0]});
-        }).catch(function(e) {
-            console.log(e);
-        });
+        await cpTokenContract.finishMinting({from: web3.eth.accounts[0]});
+        await cpTokenContract.endSale({from: web3.eth.accounts[0]});
+        await cpTokenContract.approve(creditProtocolContract.address, web3.toWei(100), {from: web3.eth.accounts[0]});
+        await creditProtocolContract.createAndStakeUcac( lndrUsdContract.address
+                                                       , usd
+                                                       , web3.toWei(100)
+                                                       , {from: web3.eth.accounts[0]});
     }
 };

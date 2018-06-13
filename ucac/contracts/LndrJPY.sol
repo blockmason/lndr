cbddf1d04c11ad308bf73cb2d4695c33af6d9b2f
Devin Canterberry <devin@twuni.org>
2018-06-13 16:27:03 -0700

    🎨 Modernize contracts; update dependencies; fix JS security vulnerabilities

--- ucac/contracts/LndrJPY.sol
+++ ucac/contracts/LndrJPY.sol
@@ -1,7 +1,7 @@
-pragma solidity 0.4.15;
+pragma solidity ^0.4.24;
 
 contract LndrJPY {
-    function allowTransaction(address creditor, address debtor, uint256 amount) public returns (bool) {
+    function allowTransaction(address, address, uint256) public pure returns (bool) {
         return true;
     }
 }

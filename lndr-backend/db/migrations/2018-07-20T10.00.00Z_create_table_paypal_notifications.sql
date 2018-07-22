dfd9ea109e613c7ee0d98437b58c0ee4ba22c220
Will Galebach <willbach@gmail.com>
2018-07-22 14:49:41 +0000

    Fixed indentation on migration file

--- lndr-backend/db/migrations/2018-07-20T10.00.00Z_create_table_paypal_notifications.sql
+++ lndr-backend/db/migrations/2018-07-20T10.00.00Z_create_table_paypal_notifications.sql
@@ -1,5 +1,5 @@
 CREATE TABLE paypal_requests (
-    friend                      CHAR(40) PRIMARY KEY,
-    requestor                   CHAR(40),
-    created_at                  TIMESTAMP DEFAULT now()
+    friend      CHAR(40) PRIMARY KEY,
+    requestor   CHAR(40),
+    created_at  TIMESTAMP DEFAULT now()
 );

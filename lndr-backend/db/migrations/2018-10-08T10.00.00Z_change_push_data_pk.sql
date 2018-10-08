d1361e632e3c6b284fe49061efd0e8198439daa7
Will Galebach <willbach@gmail.com>
2018-10-08 22:40:48 +0200

    Updated push_data migration to change data type of channel_id

--- lndr-backend/db/migrations/2018-10-08T10.00.00Z_change_push_data_pk.sql
+++ lndr-backend/db/migrations/2018-10-08T10.00.00Z_change_push_data_pk.sql
@@ -1,2 +1,6 @@
+ALTER TABLE push_data ADD COLUMN new_channel_id CHAR(36);
+UPDATE push_data SET new_channel_id = channel_id;
+ALTER TABLE push_data DROP COLUMN channel_id;
+ALTER TABLE push_data RENAME COLUMN new_channel_id TO channel_id;
 ALTER TABLE push_data DROP CONSTRAINT push_data_pkey;
 ALTER TABLE push_data ADD PRIMARY KEY (channel_id);
